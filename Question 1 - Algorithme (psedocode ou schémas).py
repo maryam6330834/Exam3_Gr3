d65dfd1b9c97440963bca1209cf85b36aba712fd
info1 <6330834@cegepoutaouais.qc.ca>
2025-11-18 15:59:55 -0500

    psedocode question 1 test unitaire rendez-vous 1

---
+++ Question 1 - Algorithme (psedocode ou schémas).py
@@ -0,0 +1,10 @@
+#Algorithme (psedocode ou schémas)
+"""
+Pour chaque caractère (c) dans le message :
+    Pour chaque sous-liste dans cle_grp3:
+        si la lettre est dans la sous liste (i):
+            remplacer c par un chiffre de la position de la sous-liste (i)
+        Sinon :
+            si elle n'existe pas garder le (c)
+"""
+
---
+++ Question 2 - Tests unitaires et débogage.py
@@ -0,0 +1,10 @@
+import debogue_moi
+import pytest
+
+def test_ajouter_apres_dernier():
+    #Arrange
+    nom = "Amélie"
+    temps = 45
+    #Act
+    confirmation = ajouter_apres_dernier(calendrier)
+
